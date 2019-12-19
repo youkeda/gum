050997283efdd2448e150a22e5960cd6e30caf1f
jiaoge <yingjunjiao@outlook.com>
2019-12-20 00:03:05 +0800

    fix

---
+++ types/components/Editor.vue.d.ts
@@ -0,0 +1,9 @@
+import { Vue } from "vue-property-decorator";
+import * as monaco from "monaco-editor";
+export default class GumEditor extends Vue {
+    editor: monaco.editor.IStandaloneCodeEditor;
+    $refs: {
+        editor: HTMLElement;
+    };
+    mounted(): void;
+}
