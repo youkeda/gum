a5af6eecb5ffe16f55864a7ea59741f5b26846c4
jiaoge <yingjunjiao@outlook.com>
2019-12-20 00:33:23 +0800

    fix

---
+++ types/index.js
@@ -0,0 +1,18 @@
+import GumEditor from "./components/Editor.vue";
+/**
+ * Fügt eine "install" function bei MLiveForce hinzu
+ *
+ * Weitere Infos:
+ *      https://vuejs.org/v2/cookbook/packaging-sfc-for-npm.html#Packaging-Components-for-npm
+ */
+const GumEditorElements = {
+    install(vue) {
+        vue.component("GumEditor", GumEditor);
+    }
+};
+if (typeof window !== "undefined" && window.Vue) {
+    // @ts-ignore
+    window.Vue.use(GumEditorElements, {});
+}
+export { GumEditor };
+export default GumEditorElements;
