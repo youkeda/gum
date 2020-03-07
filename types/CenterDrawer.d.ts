c45b1ce1b18b5ab104e505516302e3747b7dd4a7
zhoufan <zhoufan@qingguatang.com>
2020-03-07 12:08:15 +0800

    feat: fixed

--- types/CenterDrawer.d.ts
+++ types/CenterDrawer.d.ts
@@ -1,5 +1,3 @@
 import { YKDGumUI } from './Component';
 
 export declare class YKDCenterDrawer extends YKDGumUI {}
-
-export class Alert extends ElAlert {}
--- types/JSConsole.d.ts
+++ types/JSConsole.d.ts
@@ -1,5 +1,5 @@
 import { YKDGumUI } from './Component';
 
-export declare class YKDModal extends YKDGumUI {}
+export declare class JSConsole extends YKDGumUI {}
 
 export function parseWrapper(o: any): any;
