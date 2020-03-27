3be0c735541464926c5e43594ef0dcc749232418
zhoufan <zhoufan@qingguatang.com>
2020-03-27 11:28:52 +0800

    feat: fixed

--- src/components/CenterDrawer/CenterDrawer.vue
+++ src/components/CenterDrawer/CenterDrawer.vue
@@ -4,7 +4,7 @@
       <a-modal
         class="delete-topic-bank-modal"
         :title="title"
-        v-model="visible"
+        :visible="localVisible"
         @cancel="handleCancel"
         :footer="null"
         :centered="true"
@@ -21,7 +21,7 @@
       <a-modal
         class="topic-number"
         title="题目数量"
-        v-model="visible"
+        :visible="localVisible"
         @cancel="handleCancel"
         :footer="null"
       >
@@ -46,11 +46,13 @@ import { Modal } from "ant-design-vue";
   }
 })
 export default class CenterDrawer extends Vue {
-  @Prop() private visible?: boolean;
+  @Prop() private visible!: boolean;
   @Prop() private title?: string;
   @Prop() private type?: string;
   private isShow: boolean = true;
+  private localVisible: boolean = false;
   async mounted() {
+    this.localVisible = this.visible;
     if (this.type === "warn") {
       this.isShow = true;
     }
@@ -58,6 +60,11 @@ export default class CenterDrawer extends Vue {
       this.isShow = false;
     }
   }
+  @Watch("visible")
+  onVisibleChange() {
+    this.localVisible = this.visible;
+  }
+
   showModal() {
     this.$emit("open");
   }
--- test-types/components/CenterDrawer/CenterDrawer.vue.d.ts
+++ test-types/components/CenterDrawer/CenterDrawer.vue.d.ts
@@ -1,10 +1,12 @@
 import { Vue } from "vue-property-decorator";
 export default class CenterDrawer extends Vue {
-    private visible?;
+    private visible;
     private title?;
     private type?;
     private isShow;
+    private localVisible;
     mounted(): Promise<void>;
+    onVisibleChange(): void;
     showModal(): void;
     handleCancel(): void;
 }
