c67c260c3701aab69dcc0ad1419573318107579f
zfha <545227212@qq.com>
2020-05-08 12:43:15 +0800

    feat: fixed

--- test-types/src/components/JSConsole/components/PromiseType.vue.d.ts
+++ test-types/src/components/JSConsole/components/PromiseType.vue.d.ts
@@ -7,7 +7,7 @@ export default class JCPromiseType extends Vue {
     private localOpen;
     private promiseValue;
     private status;
-    whichType(value: any): "jc-string" | "jc-number" | "jc-boolean" | "jc-set" | "jc-promise" | "jc-error" | "jc-undefined" | "jc-null" | "jc-array" | "jc-function" | "jc-wrapper" | "jc-object";
+    whichType(value: any): "jc-array" | "jc-string" | "jc-null" | "jc-undefined" | "jc-number" | "jc-function" | "jc-boolean" | "jc-object" | "jc-error" | "jc-promise" | "jc-wrapper" | "jc-set";
     mounted(): void;
     toggle(e: any): void;
     init(): Promise<void>;
